61227f63215098697ffb4f69c6e8a32604798e21
Nguyen Tan Phuong <37770098+Phuongaz@users.noreply.github.com>
2023-11-20 22:31:42 +0700

    missing file

---
+++ resources/sqlite.sql
@@ -0,0 +1,23 @@
+--# !sqlite
+--# cards {
+--#  init {
+CREATE TABLE IF NOT EXISTS cards (player TEXT, amount INTEGER, telco TEXT, time TEXT)
+--#  }
+--#  insert {
+--#     :player string
+--#     :amount number
+--#     :telco string
+--#     :time string
+INSERT INTO cards (player, amount, telco, time) VALUES (:player, :amount, :telco, :pin, :serial, :time)
+--#   }
+--#  select {
+--#     :player string
+SELECT * FROM cards WHERE player = :player
+--#   }
+--#   select_top {
+SELECT player, SUM(amount) AS total_amount
+FROM cards
+GROUP BY player
+ORDER BY total_amount DESC LIMIT 10;
+--#   }
+--# }
